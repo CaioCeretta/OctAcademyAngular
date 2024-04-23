da54ef47451846d4dddf78d02ff16407cd7bd7b6
Caio Ceretta <caioceretta@gmail.com>
2024-04-23 00:13:17 -0300

    Added new fields to the form

--- AngIntro/src/app/app.component.ts
+++ AngIntro/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -7,10 +7,11 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {k
+export class AppComponent {
 
-  form: any;
+  form: FormGroup;
   emailRegex = '[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$'
+  contactRegex: string = "[789][0-9]{9}"
 
   constructor() {
     this.form = new FormGroup({
@@ -22,9 +23,23 @@ export class AppComponent {k
         Validators.required,
         Validators.pattern(this.emailRegex)
       ]),
-      address: new FormControl('', [
-        Validators.required
-      ])
+
+      contactDetails: new FormGroup({
+        address: new FormControl('', [
+          Validators.required
+        ]),
+        shippingAddress: new FormControl('', [
+          Validators.required
+        ]),
+        contactNo: new FormControl('', [
+          Validators.required,
+          Validators.pattern(this.contactRegex)
+        ])
+      }),
+
+      skills: new FormArray([])
+
+
     })
   }
 
@@ -36,14 +51,39 @@ export class AppComponent {k
     return this.form.get('email')
   }
 
+
   get address() {
-    return this.form.get('address')
+    return this.form.get('contactDetails.address');
+  }
+
+  get shippingAddress() {
+    return this.form.get('contactDetails.shippingAddress')
+  }
+
+  get contactNo() {
+    return this.form.get('contactDetails.contactNo')
+  }
+
+  get skills() {
+    return this.form.get('skills') as FormArray
+  }
+
+  addSkills(skills: HTMLInputElement) {
+    const skill = new FormControl(skills.value)
+
+    this.skills.push(
+      skill
+    )
+
+    skills.value = ''
   }
 
   onSubmit() {
     const values = this.form.value
 
     console.log(values)
+
+
 
 
 
